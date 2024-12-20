25dbfad2b6a230766170ca9bc0522fe53e0c5fe6
Miguel Palau Zarza <mpalauzarza@gmail.com>
2024-12-19 22:24:26 -0600

    also handle zip extraction for internet archive

--- arr/scripts/extract.sh
+++ arr/scripts/extract.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-if ls "$1"/*.rar 1> /dev/null 2>&1; then
-    unrar x "$1"/*.rar "$1"
+
+# Handle RAR files
+if ls "$1"/*.rar 1>/dev/null 2>&1; then
+  unrar x "$1"/*.rar "$1"
+fi
+
+# Handle ZIP files
+if ls "$1"/*.zip 1>/dev/null 2>&1; then
+  unzip "$1"/*.zip -d "$1"
 fi
